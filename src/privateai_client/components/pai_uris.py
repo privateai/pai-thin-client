fd683c0fbea084fa02ea8de87268515ba8ea1dc3
mrplt-pai <murat@private-ai.com>
2023-05-23 11:48:59 -0400

    Code formatting set-up

--- src/privateai_client/components/pai_uris.py
+++ src/privateai_client/components/pai_uris.py
@@ -1,19 +1,18 @@
-
-
 class PAIURIs:
-
     def __init__(self, scheme, host, port=None):
-        self.valid_schemes = ['http', 'https']
-        scheme = scheme.split('://')[0]
+        self.valid_schemes = ["http", "https"]
+        scheme = scheme.split("://")[0]
         if scheme not in self.valid_schemes:
-            raise ValueError(f"Scheme must be one of the following: {', '.join(self.valid_schemes)}")
+            raise ValueError(
+                f"Scheme must be one of the following: {', '.join(self.valid_schemes)}"
+            )
         port = f":{port}" if port else ""
         self._pai_uri = f"{scheme}://{host}{port}"
 
     @property
     def pai_uri(self):
         return self._pai_uri
-    
+
     @property
     def api_version(self):
         return "v3"
@@ -25,22 +24,26 @@ class PAIURIs:
     @property
     def health(self):
         return self._create_uri(self.pai_uri, "healthz")
-    
+
     @property
     def metrics(self):
         return self._create_uri(self.pai_uri, "metrics")
-    
+
     @property
     def process_text(self):
         return self._create_uri(self.pai_uri, self.api_version, "process", "text")
-    
+
     @property
     def process_files_uri(self):
-        return self._create_uri(self.pai_uri, self.api_version, "process", "files", "uri")
-    
+        return self._create_uri(
+            self.pai_uri, self.api_version, "process", "files", "uri"
+        )
+
     @property
     def process_files_base64(self):
-        return self._create_uri(self.pai_uri, self.api_version, "process", "files", "base64")
+        return self._create_uri(
+            self.pai_uri, self.api_version, "process", "files", "base64"
+        )
 
     @property
     def version(self):
@@ -48,4 +51,3 @@ class PAIURIs:
 
     def _create_uri(self, *args):
         return "/".join([x.strip("/") for x in args])
-        
