3ca98b8934b51cfabd9f8c0541b491e0ff8e0606
zsolt-pai <132347715+zsolt-pai@users.noreply.github.com>
2023-12-14 12:14:04 +0100

    [DEID-2187] check version compatibility (#29)
    Co-authored-by: Adam <122292127+a-guiducci@users.noreply.github.com>

--- src/privateai_client/pai_client.py
+++ src/privateai_client/pai_client.py
@@ -4,6 +4,7 @@ from typing import Union
 from requests import HTTPError
 
 from .components import *
+from .__about__ import __version__
 
 
 class PAIClient:
@@ -20,13 +21,14 @@ class PAIClient:
         **kwargs,
     ):
         # Add source url
-        self.uris = PAIURIs(url, scheme, host, port)
-        self.get = PAIGetRequests(self.uris)
-        self.post = PAIPostRequests(self.uris)
+        self._uris = PAIURIs(url, scheme, host, port)
+        self.get = PAIGetRequests(self._uris)
+        self.post = PAIPostRequests(self._uris)
         if "api_key" in kwargs.keys():
             self.add_api_key(kwargs["api_key"])
         elif "bearer_token" in kwargs.keys():
             self.add_bearer_token(kwargs["bearer_token"])
+        self._container_version = None
 
     def _add_auth(self, auth_type, auth_val):
         auth_header = {}
@@ -40,6 +42,23 @@ class PAIClient:
             )
         for subclass in [self.get, self.post]:
             subclass.headers = {**auth_header, **subclass.base_header}
+
+    def _version_warning(self) -> None:
+
+        # only compare major and minor version numbers
+        parsed_container_version = self._container_version.split('.')[:2]
+        parsed_client_version = __version__.split('.')[:2]
+
+        if parsed_container_version != parsed_client_version:
+            logging.warning(f"Version mismatch: privateai_client {__version__} may be incompatible with PAI container "
+                            f"{self._container_version}. Please install the appropriate client version!")
+
+    def check_version_compatibility(self) -> None:
+
+        if self._container_version is None:
+            self.get_version()
+        else:
+            self._version_warning()
 
     def add_api_key(self, api_key: str):
         self._add_auth("api_key", api_key)
@@ -62,18 +81,23 @@ class PAIClient:
         """
         Returns information about the Private-AI's server
         """
+        self.check_version_compatibility()
         return MetricsResponse(self.get.metrics())
 
     def get_version(self):
         """
         Returns the version of the container application code
         """
-        return VersionResponse(self.get.version())
+        ret = VersionResponse(self.get.version())
+        self._container_version = ret.app_version
+        self._version_warning()
+        return ret
 
     def get_diagnostics(self):
         """
         Returns diagnostic information about the Private-AI container host
         """
+        self.check_version_compatibility()
         return DiagnosticResponse(self.get.diagnostics())
 
     def process_text(self, request_object: Union[dict, ProcessTextRequest]):
@@ -81,8 +105,10 @@ class PAIClient:
         Used to deidentify text
         """
         if type(request_object) is ProcessTextRequest:
+            self.check_version_compatibility()
             response = TextResponse(self.post.process_text(request_object.to_dict()))
         elif type(request_object) is dict:
+            self.check_version_compatibility()
             response = TextResponse(self.post.process_text(request_object))
         else:
             raise ValueError(
@@ -95,11 +121,13 @@ class PAIClient:
         Used to reidentify text
         """
         if type(request_object) is ReidentifyTextRequest:
+            self.check_version_compatibility()
             response = ReidentifyTextResponse(
                 self.post.reidentify_text(request_object.to_dict())
             )
         elif type(request_object) is dict:
-            response = ReidentifyTextRequest(self.post.reidentify_text(request_object))
+            self.check_version_compatibility()
+            response = ReidentifyTextResponse(self.post.reidentify_text(request_object))
         else:
             raise ValueError(
                 "request_object can only be a dictionary or a ReidentifyTextRequest object"
@@ -111,10 +139,12 @@ class PAIClient:
         Used to deidentify files by uri
         """
         if type(request_object) is ProcessFileUriRequest:
+            self.check_version_compatibility()
             response = FilesUriResponse(
                 self.post.process_files_uri(request_object.to_dict())
             )
         elif type(request_object) is dict:
+            self.check_version_compatibility()
             response = FilesUriResponse(self.post.process_files_uri(request_object))
         else:
             raise ValueError(
@@ -129,10 +159,12 @@ class PAIClient:
         Used to deidentify base64 files
         """
         if type(request_object) is ProcessFileBase64Request:
+            self.check_version_compatibility()
             response = FilesBase64Response(
                 self.post.process_files_base64(request_object.to_dict())
             )
         elif type(request_object) is dict:
+            self.check_version_compatibility()
             response = FilesBase64Response(
                 self.post.process_files_base64(request_object)
             )
@@ -147,8 +179,10 @@ class PAIClient:
         Used to deidentify audio files by uri
         """
         if type(request_object) is BleepRequest:
+            self.check_version_compatibility()
             response = BleepResponse(self.post.bleep(request_object.to_dict()))
         elif type(request_object) is dict:
+            self.check_version_compatibility()
             response = BleepResponse(self.post.bleep(request_object))
         else:
             raise ValueError(
