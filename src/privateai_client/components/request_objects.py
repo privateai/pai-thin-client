cc3014a4d61379549f9b735cca2e1ffeacdf785a
Shaik Amir <amir@private-ai.com>
2024-08-05 04:44:23 -0400

    ImageOptions class

--- src/privateai_client/components/request_objects.py
+++ src/privateai_client/components/request_objects.py
@@ -272,7 +272,7 @@ class File(BaseRequestObject):
         "application/pdf",
         "application/vnd.ms-excel",
         "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-        "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
         "application/vnd.openxmlformats-officedocument.presentationml.presentation",
         "application/xml",
         "message/rfc822",
